b4b00a9e83315148a046e647d1fbaa02dc023b03
AlexTraveylan <monsieurdemares@gmail.com>
2023-05-13 09:45:36 +0200

    dfjklgdf

--- components/caroussel.tsx
+++ components/caroussel.tsx
@@ -48,6 +48,7 @@ export function Carroussel({ images }: { images: imageCaroussel[] }) {
                     width: 'auto',
                     objectFit: 'cover',
                   }}
+                  priority={currentIndex != 0}
                 />
               </div>
               {image.titleContent && image.textContent && (
